08a6fc6c3c1f44e96927069ef7bef5583c8458dc
Fernando <fernandoavilajunior@gmail.com>
2023-04-26 23:30:57 -0300

    Christmas Lights finished

--- 4-ChristmasLights/main.js
+++ 4-ChristmasLights/main.js
@@ -1,44 +1,44 @@
-const pattern = [
-	["o", "x", "o", "x", "o"],
-	["x", "o", "x", "o", "x"],
-	["o", "x", "o", "x", "o"],
-	["x", "o", "x", "o", "x"],
-	["o", "x", "o", "x", "o"],
-];
-
 const lights = document.querySelectorAll(".light-grid .light");
+const time = document.querySelector('input[name="time"]');
 
-const lightPattern = [];
+document.querySelector("#time-value").innerHTML = time.value;
 
-function drawPattern() {
-    for (let i = 0; i < pattern.length; i++) {
-		let line = [];
-		for (let j = 0; j < pattern[i].length; j++) {
-			lightPattern.push(lights[j + i * pattern[i].length]);
+time.addEventListener("change", (event) => {
+	updateValues();
+	document.querySelector("#time-value").innerHTML = event.target.value;
+});
+
+updateValues();
+
+function updateValues() {
+	lights.forEach( light => {
+		let animationTime = parseFloat((time.value * 4) / 10);
+		
+		if (light.classList.contains("green")) {
+			animationTime = parseFloat((time.value * 5) / 10);
 		}
-    }
-    
-    for (let i = 0; i < pattern.length; i++) {
-		for (let j = 0; j < pattern[i].length; j++) {
-            if (pattern[i][j] == "o") {
-                let color =
-                    "#" + Math.floor(Math.random() * 16777215).toString(16);
-                let item = lightPattern[j + i * pattern[i].length];
-                item.classList.add("light-on");
-                item.style.backgroundColor = color;
-                item.style.borderColor = color;
-                item.style.boxShadow =
-					"0 10px 10px rgb(255, 255, 255), 0 10px 20px rgb(255, 255, 255), 0 10px 30px rgb(255, 255, 255), 0 10px 40px rgb(255, 255, 255), 0 10px 50px rgb(255, 255, 255), 0 10px 70px " +
-					color +
-					", 0 10px 80px " +
-					color +
-					", 0 10px 100px " +
-					color +
-					", 0 10px 150px " +
-					color;
-            }
+
+		if (light.classList.contains("blue")) {
+			animationTime = parseFloat((time.value * 6) / 10);
 		}
-	}
+
+		if (light.classList.contains("red")) {
+			animationTime = parseFloat((time.value * 8) / 10);
+		}
+
+		light.style.animationDuration = animationTime +"s";
+	});
 }
 
-drawPattern();
+function basicControll(key) {
+	console.log(key);
+	if (key == 'on') {
+		lights.forEach((light) => {
+			light.classList.remove("turn-off");
+		});
+	} else {
+		lights.forEach((light) => {
+			light.classList.add("turn-off");
+		});
+	}
+}
