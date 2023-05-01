ef944b802e07cfaa46d3019d6231b8cc2c3cc68f
Fernando <fernandoavilajunior@gmail.com>
2023-05-01 03:49:24 -0300

    CountdownTimer project finished

--- 7-CountdownTimer/README.md
+++ 7-CountdownTimer/README.md
@@ -23,39 +23,34 @@ own original implementation.
 
 ## User Stories
 
--   [ ] User can see an event input box containing an event name field, an
+-   [x] User can see an event input box containing an event name field, an
 date field, an optional time, and a 'Start' button.
--   [ ] User can define the event by entering its name, the date it is
+-   [x] User can define the event by entering its name, the date it is
 scheduled to take place, and an optional time of the event. If the time is 
 omitted it is assumed to be at Midnight on the event date in the local time
 zone.
--   [ ] User can see a warning message if the event name is blank.
--   [ ] User can see a warning message if the event date or time are incorrectly
+-   [x] User can see a warning message if the event name is blank.
+-   [x] User can see a warning message if the event date or time are incorrectly
 entered. 
--   [ ] User can see a warning message if the time until the event data and time
+-   [x] User can see a warning message if the time until the event data and time
 that has been entered would overflow the precision of the countdown timer.
--   [ ] User can click on the 'Start' button to see the countdown timer start
+-   [x] User can click on the 'Start' button to see the countdown timer start
 displaying the days, hours, minutes, and seconds until the event takes place.
--   [ ] User can see the elements in the countdown timer automatically
+-   [x] User can see the elements in the countdown timer automatically
 decrement. For example, when the remaining seconds count reaches 0 the remaining
 minutes count will decrement by 1 and the seconds will start to countdown from 59. This progression must take place from seconds all the way up to the remaining days position in countdown display. 
 
 ## Bonus features
 
--   [ ] User can save the event so that it persists across sessions
--   [ ] User can see an alert when the event is reached
--   [ ] User can specify more than one event. 
--   [ ] User can see a countdown timers for each event that has been defined.
+-   [x] User can save the event so that it persists across sessions
+-   [x] User can see an alert when the event is reached
+-   [x] User can specify more than one event. 
+-   [x] User can see a countdown timers for each event that has been defined.
 
-## Useful links and resources
+## Example project
 
-- Images of analog tube-based countdown timers can be found 
-[here](https://nixieshop.com/)
-- [`clearInterval` MDN](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval)
-- [`setInterval` MDN](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval)
-- [Date MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date)
+[Countdown Timer App](http://ofernandoavila.avilamidia.com/challenges/7-CountdownTimer/)
 
-## Example projects
+## Versions
 
-[Simple Clock/Countdown Timer](https://codepen.io/karlo-stekovic/pen/OajKVK)
-[Countdown Timer built with React](https://www.florin-pop.com/blog/2019/05/countdown-built-with-react/)
+[ 01/05/2023 ] - v1.0.0 - Finished project 
--- 7-CountdownTimer/main.js
+++ 7-CountdownTimer/main.js
@@ -1,198 +1,261 @@
 let isPaused = true;
-let initialTime = '';
-let timer = '';
-let counterList = GetFromBrowser('countdown_timer_itens') ? GetFromBrowser('countdown_timer_itens') : [];
+let initialTime = "";
+let timer = "";
+let counterList = GetFromBrowser("countdown_timer_itens")
+	? GetFromBrowser("countdown_timer_itens")
+	: [];
 let dateMode = true;
 
 function Init() {
-    if (isChecked('startNow')) {
-        ToggleField('initialDate');
-    }
+	if (isChecked("startNow")) {
+		ToggleField("initialDate");
+	}
 
-    if(counterList.length > 0) {
-        counterList.forEach( item => {
-            let container = CreateTimerItem(item);
-            item.setIntervalId = StartTimer(
-                item, 
-                container.querySelector('span.counter')
-            );
-        });
-    } else {
-        document.getElementById('clearlist_button').disabled = true;
-    }
+	if (counterList.length > 0) {
+		counterList.forEach((item) => {
+			let container = CreateTimerItem(item);
+			item.setIntervalId = StartTimer(
+				item,
+				container.querySelector("span.counter"),
+			);
+		});
+	} else {
+		document.getElementById("clearlist_button").disabled = true;
+	}
 }
 
 Init();
 
 function StartTimer(item, location) {
-    time = DiffDates(new Date(), new Date(item.endDate));
-    DefineVisor(time, location);
+	time = DiffDates(new Date(), new Date(item.endDate));
+	DefineVisor(time, location);
+	if (time.second <= 0) {
+		DefineVisor(
+			{
+				day: 0,
+				hour: 0,
+				minute: 0,
+				second: 0,
+			},
+			location,
+		);
+		return;
+	}
 
-    console.log(time);
-    if (time.second <= 0) {
-        DefineVisor({
-            day: 0,
-			hour: 0,
-			minute: 0,
-			second: 0,
-        }, location);
-        return;
-    }
+	let counterInterval = setInterval(() => {
+		time = DiffDates(new Date(), new Date(item.endDate));
+		DefineVisor(time, location);
+		if (time.second == 0) {
+			if (time.minute >= 1) {
+				time.minute--;
+				time.second = 59;
+			} else {
+				if (time.hour >= 1) {
+					time.hour--;
+					time.minute = 59;
+					time.second = 59;
+				} else {
+					if (time.day >= 1) {
+						time.day--;
+						time.hour = 23;
+						time.minute = 59;
+						time.second = 59;
+					}
+				}
+			}
+		} else {
+			time.second--;
+		}
+		DefineVisor(time, location);
+		if (
+			time.hour == 0 &&
+			time.minute == 0 &&
+			time.second == 0 &&
+			time.day == 0
+		)
+			FinishTimer(counterInterval, item);
+	}, 1000);
 
-    let counterInterval = setInterval(() => {
-        time = DiffDates(new Date(), new Date(item.endDate));
-        DefineVisor(time, location);
-        if (time.second == 0) {
-            if (time.minute >= 1) {
-                time.minute--;
-                time.second = 59;
-            } else {
-                if (time.hour >= 1) {
-                    time.hour--;
-                    time.minute = 59;
-                    time.second = 59;
-                } else {
-                    if (time.day >= 1) {
-                        time.day--;
-                        time.hour = 23;
-                        time.minute = 59;
-                        time.second = 59;
-                    }
-                }
-            }
-        } else {
-            time.second--;
-        }
-        DefineVisor(time, location);
-        if (time.hour == 0 && time.minute == 0 && time.second == 0 && time.day == 0) FinishTimer(counterInterval);
-    }, 1000);
-
-    return counterInterval;
+	return counterInterval;
 }
 
-function FinishTimer(id) {
+function FinishTimer(id, item = '') {
     clearInterval(id);
+    if (item != '') SendAlert(`Your counter "${item.name}" is over!`);
 }
 
 function DeleteItemButton(id) {
-    let key = 0;
-    if(counterList.length > 1) key = id - 1;
-    let item = counterList[key];
-    console.log(item);
-    document.getElementById('timer-item-' + item.timerId).remove();
+	let item = counterList[id];
+	FinishTimer(item.timerId);
 
-    let out = [];
-    counterList.forEach( item => {
-        if(item.timerId != id) out.push(item);
-    });
+	document.getElementById("timer-item-" + item.timerId).remove();
 
-    console.log(out);
-    SaveInBrowser('countdown_timer_itens', out);
+	let out = [];
+	counterList.forEach((item) => {
+		if (item.timerId != id) out.push(item);
+	});
+
+	SaveInBrowser("countdown_timer_itens", out);
 }
 
-function DefineVisor(time, location = '') {
-    let day = (time.day <= 0) ? '' : time.day.toString() + ":";
-    let hour = (time.hour < 10) ? "0" + time.hour.toString() : time.hour.toString();
-    let minute = (time.minute < 10) ? "0" + time.minute.toString() : time.minute.toString();
-    let second = (time.second < 10) ? "0" + time.second.toString() : time.second.toString();
+function DefineVisor(time, location = "") {
+	let day = time.day <= 0 ? "" : time.day.toString() + ":";
+	let hour =
+		time.hour < 10 ? "0" + time.hour.toString() : time.hour.toString();
+	let minute =
+		time.minute < 10
+			? "0" + time.minute.toString()
+			: time.minute.toString();
+	let second =
+		time.second < 10
+			? "0" + time.second.toString()
+			: time.second.toString();
 
-    if(location != '') {
-        location.innerHTML = day + hour + ":" + minute + ":" + second;
-    } else {
-        document.querySelector('.counter span').innerHTML = day + hour + ":" + minute + ":" + second;
-    }
+	if (location != "") {
+		location.innerHTML = day + hour + ":" + minute + ":" + second;
+	} else {
+		document.querySelector(".counter span").innerHTML =
+			day + hour + ":" + minute + ":" + second;
+	}
 }
 
 function GetTimerItem() {
-    initialDate = document.getElementById('startNow').checked ? new Date() : new Date(document.getElementById('initialDate').value);
-    endDate = new Date(document.getElementById('endDate').value);
+	initialDate = document.getElementById("startNow").checked
+		? new Date()
+		: new Date(document.getElementById("initialDate").value);
+	endDate = new Date(document.getElementById("endDate").value);
 
-    return {
-        timerId: (counterList.length + 1),
-        name: document.getElementById('name').value,
-        initialDate,
-        endDate,
-        setIntervalId: 0
-    };
+	return {
+		timerId: counterList.length,
+		name: document.getElementById("name").value,
+		initialDate,
+		endDate,
+		setIntervalId: 0,
+	};
+}
+
+function OnToggleStartDate() {
+	ToggleField("initialDate");
+	ToggleRequired("initialDate");
+
+	let initialDate = document.getElementById("initialDate");
+
+	if ( initialDate.parentNode.classList.contains("invalid-group") && !initialDate.required ) RemoveInvalidGroup(initialDate);
 }
 
 function HandleCreateCountDownTimerItem() {
+	if (HaveInvalidFields()) return;
+
     let data = GetTimerItem();
     
-    let tmp = CreateTimerItem(data);
-    counterList.push(data);
-    SaveInBrowser('countdown_timer_itens', counterList);
+    if(data.initialDate > data.endDate) return InvalidateField(document.getElementById("endDate").parentNode, 'End date must be higher than Initial date!');
 
-    data.setIntervalId = StartTimer(
-        data, 
-        tmp.querySelector('span.counter')
-    );
+	counterList.push(data);
+	SaveInBrowser("countdown_timer_itens", counterList);
+	let tmp = CreateTimerItem(data);
+	data.setIntervalId = StartTimer(data, tmp.querySelector("span.counter"));
+
+	ClearAllInputs();
 }
 
 function CreateTimerItem(data) {
-    let timerItemContainer = createElement('li', [{ key: 'id', value: 'timer-item-' + data.timerId }]);
-    
-    let info = createElement('div', [{ key: 'class', value: ['info'] }]);
-    let header = createElement('div', [{ key: 'class', value: ['title-header'] }]);
-    
-    header.appendChild(createElement('p', [{ key: 'class', value: ['title'] }, { key: 'content', value: data.name }]));
-    
-    info.appendChild(header);
-    info.appendChild(createElement('span', [{ key: 'class', value: ['counter'] }, { key: 'content', value: '00:00:00' }]));
+	let timerItemContainer = createElement("li", [
+		{ key: "id", value: "timer-item-" + data.timerId },
+	]);
 
-    timerItemContainer.appendChild(info);
+	let info = createElement("div", [{ key: "class", value: ["info"] }]);
+	let header = createElement("div", [
+		{ key: "class", value: ["title-header"] },
+	]);
 
-    let description = createElement('div', [{ key: 'class', value: ['description'] }]);
-    let tmpDiv = createElement('div');
+	header.appendChild(
+		createElement("p", [
+			{ key: "class", value: ["title"] },
+			{ key: "content", value: data.name },
+		]),
+	);
 
-    tmpDiv.appendChild(createElement('p', [{ key: 'content', value: 'Initial time' }]));
-    tmpDiv.appendChild(createElement('span', [{ key: 'content', value: DateToString(new Date(data.initialDate)) }]));
+	info.appendChild(header);
+	info.appendChild(
+		createElement("span", [
+			{ key: "class", value: ["counter"] },
+			{ key: "content", value: "00:00:00" },
+		]),
+	);
 
-    description.appendChild(tmpDiv);
-    tmpDiv = null;
-    tmpDiv = createElement('div');
+	timerItemContainer.appendChild(info);
 
-    tmpDiv.appendChild(createElement('p', [{ key: 'content', value: 'End time' }]));
-    tmpDiv.appendChild(createElement('span', [{ key: 'content', value: DateToString(new Date(data.endDate)) }]));
-    description.appendChild(tmpDiv);
+	let description = createElement("div", [
+		{ key: "class", value: ["description"] },
+	]);
+	let tmpDiv = createElement("div");
 
-    timerItemContainer.appendChild(description);
+	tmpDiv.appendChild(
+		createElement("p", [{ key: "content", value: "Initial time" }]),
+	);
+	tmpDiv.appendChild(
+		createElement("span", [
+			{ key: "content", value: DateToString(new Date(data.initialDate)) },
+		]),
+	);
 
-    let controls = createElement('div', [{ key: 'class', value: ['controls'] }]);
+	description.appendChild(tmpDiv);
+	tmpDiv = null;
+	tmpDiv = createElement("div");
 
-    let tmpRow = createElement('div', [{ key: 'class', value: ['row'] }]);
-    let tmpColumn = createElement('div', [{ key: 'class', value: ['row'] }]);
-    let tmpInputGroup = createElement('div', [{ key: 'class', value: ['input-group'] }]);
+	tmpDiv.appendChild(
+		createElement("p", [{ key: "content", value: "End time" }]),
+	);
+	tmpDiv.appendChild(
+		createElement("span", [
+			{ key: "content", value: DateToString(new Date(data.endDate)) },
+		]),
+	);
+	description.appendChild(tmpDiv);
 
-    tmpRow = createElement('div', [{ key: 'class', value: ['row'] }]);
-    tmpColumn = createElement('div', [{ key: 'class', value: ['column'] }]);
-    tmpInputGroup = createElement('div', [{ key: 'class', value: ['input-group'] }]);
-    let timerID = data.timerId != undefined ? data.timerId : counterList.length - 1;
-    tmpInputGroup.appendChild(
-        createElement(
-            'button',
-            [
-                { key: 'class', value: ['btn', 'btn-danger'] },
-                { key: 'content', value: 'Delete' },
-                { key: 'id', value: 'delete_button' },
-                { key: 'onclick', value: 'DeleteItemButton(' + timerID + ')' }
-            ]
-        )
-    );
+	timerItemContainer.appendChild(description);
 
-    tmpColumn.appendChild(tmpInputGroup);
-    tmpRow.appendChild(tmpColumn);
-    controls.appendChild(tmpRow);
+	let controls = createElement("div", [
+		{ key: "class", value: ["controls"] },
+	]);
 
-    timerItemContainer.appendChild(controls);
-    document.getElementById('clearlist_button').disabled = false;
-    document.querySelector('.timer-list ul').appendChild(timerItemContainer);
+	let tmpRow = createElement("div", [{ key: "class", value: ["row"] }]);
+	let tmpColumn = createElement("div", [{ key: "class", value: ["row"] }]);
+	let tmpInputGroup = createElement("div", [
+		{ key: "class", value: ["input-group"] },
+	]);
 
-    return timerItemContainer;
+	tmpRow = createElement("div", [{ key: "class", value: ["row"] }]);
+	tmpColumn = createElement("div", [{ key: "class", value: ["column"] }]);
+	tmpInputGroup = createElement("div", [
+		{ key: "class", value: ["input-group"] },
+	]);
+
+	let timerID =
+		data.timerId != undefined ? data.timerId : counterList.length - 1;
+
+	tmpInputGroup.appendChild(
+		createElement("button", [
+			{ key: "class", value: ["btn", "btn-danger"] },
+			{ key: "content", value: "Delete" },
+			{ key: "id", value: "delete_button" },
+			{ key: "onclick", value: "DeleteItemButton(" + timerID + ")" },
+		]),
+	);
+
+	tmpColumn.appendChild(tmpInputGroup);
+	tmpRow.appendChild(tmpColumn);
+	controls.appendChild(tmpRow);
+
+	timerItemContainer.appendChild(controls);
+	document.getElementById("clearlist_button").disabled = false;
+	document.querySelector(".timer-list ul").appendChild(timerItemContainer);
+
+	return timerItemContainer;
 }
 
 function HandleClearTimerList() {
-    SaveInBrowser('countdown_timer_itens', []);
-    document.querySelector('.timer-list ul').innerHTML = '';
-    document.getElementById('clearlist_button').disabled = true;
+	SaveInBrowser("countdown_timer_itens", []);
+	document.querySelector(".timer-list ul").innerHTML = "";
+	document.getElementById("clearlist_button").disabled = true;
 }
