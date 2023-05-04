443381b05964861bb31698ad1e39f0e7f6269c62
Fernando <fernandoavilajunior@gmail.com>
2023-05-04 01:38:26 -0300

    ColorCycle project update

--- 6-ColorCycle/README.md
+++ 6-ColorCycle/README.md
@@ -14,15 +14,15 @@ an RGB color specification (e.g. `#000000` color code).
 
 ## User Stories
 
--   [ ] User can specify a starting fill color as a six hexadecimal standard
+-   [x] User can specify a starting fill color as a six hexadecimal standard
         CSS color code in three individual components of two digits each - red,
         blue, and green
 -   [ ] User can specify an increment value for each color component that will
         be added to that component every .25 second
--   [ ] User can see the box containing the fill color change every .25 seconds
--   [ ] User can only change the color components and their increments when
+-   [x] User can see the box containing the fill color change every .25 seconds
+-   [x] User can only change the color components and their increments when
         the app is stopped
--   [ ] User can start and stop the fill operation using a button whose name
+-   [x] User can start and stop the fill operation using a button whose name
         changes to 'Start' when stopped and 'Stop' when started
 -   [ ] User will receive an warning if something other than hexadecimal digits
         are entered for the color components
@@ -31,10 +31,6 @@ an RGB color specification (e.g. `#000000` color code).
 
 -   [ ] User can change the time interval between color changes
 -   [ ] User can specify the color encoding format used from RGB to another format like HSL
-
-## Useful links and resources
-
-[CSS Color Codes](https://qhmit.com/css/css_color_codes.cfm)
 
 ## Example projects
 
--- 6-ColorCycle/main.js
+++ 6-ColorCycle/main.js
@@ -1,11 +1,99 @@
 let isRunning = false;
+let colors = [];
+let currentId = 2;
+
+function StartOnLoadPage() {
+    GetColors();
+}
 
 function StartStopAnimation() {
-    if (isRunning) {
-        SwitchButton(document.querySelector("#start-stop-button"), "stop");
-    } else {
-		SwitchButton(document.querySelector("#start-stop-button"), "start");
-    }
-    
     isRunning = !isRunning;
+
+    if (isRunning) {
+        StartAnimation();
+		SwitchButton(document.querySelector("#start-stop-button"), "stop");
+    } else {
+        StopAnimation();
+		SwitchButton(document.querySelector("#start-stop-button"), "start");
+	}
+
+}
+
+function StartAnimation() {    
+    GetColors();
+    document.querySelector('.preview').style.animation = 'switch-' + parseInt(colors.length - 1).toString() + ' .25s linear infinite';
+
+    ToggleDisabledAll(document.querySelectorAll('input[type="color"]'));
+    ToggleDisabledAll(document.querySelectorAll('#color-list button'));
+    ToggleDisabled("#add-color-button");
+}
+
+function StopAnimation() {
+    document.querySelector('.preview').style.animation = '';
+    ToggleDisabledAll(document.querySelectorAll('input[type="color"]'));
+    ToggleDisabledAll(document.querySelectorAll("#color-list button"));
+    ToggleDisabled("#add-color-button");
+}
+
+function AddColor() {
+    if (colors.length >= 5) return;
+    currentId++;
+
+	const colorListElements = document.getElementById("color-list");
+	const nextColorNumber = currentId;
+
+	colorListElements.appendChild(
+		createElement("color-picker", {
+			colorId: nextColorNumber,
+			inputConfig: [
+				{ key: "name", value: "color-" + nextColorNumber },
+                { key: "id", value: "color-" + nextColorNumber },
+                { key: 'onchange', value: 'GetColors()' }
+			],
+			labelConfig: [
+				{ key: "for", value: "color-" + nextColorNumber },
+				{ key: "content", value: "Color " + nextColorNumber },
+			],
+			buttonConfig: [
+				{
+					key: "class",
+					value: ["btn", "btn-danger", "w-10", "mg-left-auto"],
+				},
+				{ key: "value", value: "remove" },
+				{
+					key: "onclick",
+					value: "RemoveColor(" + nextColorNumber + ")",
+				},
+			],
+		}),
+	);
+
+	GetColors();
+}
+
+function RemoveColor(colorId) {
+    if (colors.length == 2) return;
+    document.querySelector('input[name="color-' + colorId + '"]').parentNode.remove();
+    GetColors();
+
+    ToggleAddNewColor();
+}
+
+function GetColors() {
+    colors = [];
+    
+    document.querySelectorAll('input[type="color"]').forEach((item, index) => {
+        document.documentElement.style.setProperty("--color" + parseInt(index + 1).toString(), item.value);
+		colors.push(item.value);
+	});
+
+    ToggleAddNewColor();
+
+	console.log(colors);
+}
+
+function ToggleAddNewColor() {
+    if (colors.length >= 5) ToggleDisabled("#add-color-button");
+	else if (colors.length < 5 && isDisabled("add-color-button"))
+		ToggleDisabled("#add-color-button");
 }
