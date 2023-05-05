4c69ea321e5d64b3756e30301e0a41b5728c22c9
Fernando <fernandoavilajunior@gmail.com>
2023-05-04 23:41:19 -0300

    DolarToCents bug fixes

--- 9-DollarToCents/main.js
+++ 9-DollarToCents/main.js
@@ -11,63 +11,61 @@ function StartOnLoadPage() {
 }
 
 function Convert() {
+    if (HaveInvalidFields()) return;
 	if (coinsOut.classList.contains("hidden")) ToggleVisible(coinsOut);
 
-	let amount = parseFloat(parseFloat(
-		document.getElementById("total-value").value,
-	).toFixed(2));
+	let amount = parseFloat(
+		parseFloat(document.getElementById("total-value").value).toFixed(2),
+	);
 
 	let coins = {
-		quarters: 0,    // .25
-		dimes: 0,       // .10
-		nickels: 0,     // .05
-        pennies: 0,     // .01
-        totalCoins: 0
-    };
-    
-    let current = amount;
-    let ready = false;
+		quarters: 0, // .25
+		dimes: 0, // .10
+		nickels: 0, // .05
+		pennies: 0, // .01
+		totalCoins: 0,
+	};
 
-    while (ready == false) {
-        if ((coins.quarters + 1) * .25 <= current) {
-            coins.quarters++;
-        } else {
-            if (
-                (coins.quarters * .25) +
-                (coins.dimes + 1) * .10
-                <= current
-            ) {
-                coins.dimes++;
-            } else {
-                if (
-                    coins.quarters * 0.25 +
-                    coins.dimes * 0.1 +
-                    (coins.nickels + 1) * 0.05
-                    <= current
-                ) {
-                    coins.nickels++;
-                } else {
-                    let next =
+	let current = amount;
+	let ready = false;
+
+	while (ready == false) {
+		if ((coins.quarters + 1) * 0.25 <= current) {
+			coins.quarters++;
+		} else {
+			if (coins.quarters * 0.25 + (coins.dimes + 1) * 0.1 <= current) {
+				coins.dimes++;
+			} else {
+				if (
+					coins.quarters * 0.25 +
+						coins.dimes * 0.1 +
+						(coins.nickels + 1) * 0.05 <=
+					current
+				) {
+					coins.nickels++;
+				} else {
+					let next =
 						coins.quarters * 0.25 +
 						coins.dimes * 0.1 +
 						coins.nickels * 0.05 +
-						(coins.pennies + 1) * 0.01; 
-                    if (parseFloat(next.toFixed(2)) <= current ) {
-                        coins.pennies++;
-                    } else {
-                        ready = true;
-                    }
-                }
-            }
-        }
-    }
+						(coins.pennies + 1) * 0.01;
+					if (parseFloat(next.toFixed(2)) <= current) {
+						coins.pennies++;
+					} else {
+						ready = true;
+					}
+				}
+			}
+		}
+	}
 
-    coins.totalCoins =
+	coins.totalCoins =
 		coins.quarters + coins.dimes + coins.nickels + coins.pennies;
 
-    document.querySelector('#coins-out b').innerHTML = coins.totalCoins + ' coins';
-    document.querySelector("li.quarters").innerHTML = coins.quarters;
-    document.querySelector('li.dimes').innerHTML = coins.dimes;
-    document.querySelector('li.nickels').innerHTML = coins.nickels;
-    document.querySelector('li.pennies').innerHTML = coins.pennies;
+	document.querySelector("#coins-out b").innerHTML =
+		coins.totalCoins + " coins";
+	document.querySelector("li.quarters").innerHTML = coins.quarters;
+	document.querySelector("li.dimes").innerHTML = coins.dimes;
+	document.querySelector("li.nickels").innerHTML = coins.nickels;
+	document.querySelector("li.pennies").innerHTML = coins.pennies;
 }
