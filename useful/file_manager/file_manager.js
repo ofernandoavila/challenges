83665d155235b399bcd6e50f638aa941ee30a200
Fernando Avila <fernandoavilajunior@gmail.com>
2023-05-06 22:08:49 -0300

    useful file manager created!

---
+++ useful/file_manager/file_manager.js
@@ -0,0 +1,19 @@
+async function GetFileContentAsString(path, callback) {
+    if (path == undefined || path == null || path == "")
+        throw new Error("Path cannot be null!");
+
+    await fetch(path)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error("Cannot get the file with the path given");
+
+            return response.arrayBuffer();
+        })
+        .then((buffer) => {
+            var conteudo = new TextDecoder().decode(buffer);
+    		callback(conteudo);
+        })
+        .catch((error) => {
+            console.error("An error ocurred: ", error);
+        });
+}
