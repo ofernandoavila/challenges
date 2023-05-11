6a447990d8ec1ed720e6c911748ef089b6d92d00
Fernando Avila <fernandoavilajunior@gmail.com>
2023-05-11 00:47:26 -0300

    FIrstDBApp updated!

---
+++ 11-FirstDBApp/Database.js
@@ -0,0 +1,91 @@
+class IndexedDBManager {
+    constructor(databaseName, storeName) {
+        this.databaseName = databaseName;
+        this.storeName = storeName;
+        this.db = null;
+    }
+
+    async connect() {
+        return new Promise((resolve, reject) => {
+            const request = window.indexedDB.open(this.databaseName);
+
+            request.onerror = () => {
+                AddLogMessage(
+                    new Error(
+                        `Failed to connect to database "${this.databaseName}".`
+                    )
+                );
+                reject();
+            };
+
+            request.onsuccess = () => {
+                this.db = request.result;
+                resolve();
+            };
+
+            request.onupgradeneeded = (event) => {
+                const db = event.target.result;
+
+                if (!db.objectStoreNames.contains(this.storeName)) {
+                    AddLogMessage("Creating table");
+                    db.createObjectStore(this.storeName, { keyPath: "userId" });
+                }
+            };
+        });
+    }
+
+    async insertUser(user) {
+        return new Promise((resolve, reject) => {
+            try {
+                const transaction = this.db.transaction(
+                    [this.storeName],
+                    "readwrite"
+                );
+                const store = transaction.objectStore(this.storeName, {
+                    keyPath: "userId",
+                });
+                const request = store.add(user);
+
+                request.onerror = () => {
+                    reject(
+                        new Error(`Failed to insert user "${user.userId}".`)
+                    );
+                };
+                request.onsuccess = () => {
+                    AddLogMessage("User saved!");
+                    AddNotification("User saved!", "success");
+                    resolve();
+                };
+            } catch (error) {
+                AddNotification("An error ocurred", "danger");
+                AddLogMessage(error);
+            }
+        });
+    }
+
+    async initialLoad(users) {
+        AddLogMessage("Saving load of data...");
+        return Promise.all(users.map((user) => this.insertUser(user)));
+    }
+
+    async getAllDataFromStore() {
+        return new Promise((resolve, reject) => {
+          try {
+                const transaction = this.db.transaction([this.storeName], "readonly");
+                const store = transaction.objectStore(this.storeName);
+                const request = store.getAll();
+            
+                request.onerror = () => {
+                    reject(new Error(`Failed to retrieve data from store "${this.storeName}".`));
+                };
+            
+                request.onsuccess = () => {
+                    resolve(request.result);
+                };
+            } catch (error) {
+                AddNotification("An error ocurred", "danger");
+                AddLogMessage(error);
+            }
+        }
+    );}
+}
--- 11-FirstDBApp/main.js
+++ 11-FirstDBApp/main.js
@@ -1,15 +1,39 @@
-function LoadDB() {
-	const customerData = [
-		{ userid: "444", name: "Bill", email: "bill@company.com" },
-		{ userid: "555", name: "Donna", email: "donna@home.org" },
-	];
-	let customer = new Customer(DBNAME);
-	customer.initialLoad(customerData);
+async function LoadDB() {
+	// const customerData = [
+	// 	{ userid: "444", name: "Bill", email: "bill@company.com" },
+	// 	{ userid: "555", name: "Donna", email: "donna@home.org" },
+	// ];
+	// // let customer = new Customer(DBNAME);
+	// // customer.initialLoad(customerData);
+
+	AddLogMessage("Creating Database");
+	// const dbManager = new IndexedDBManager("myDatabase", "myStore");
+
+	// await dbManager.connect();
+
+	// const users = [
+	// 	{ userId: 2, name: "Jane Smith", email: "jane.smith@example.com" },
+	// 	{ userId: 3, name: "Bob Johnson", email: "bob.johnson@example.com" },
+	// ];
+	// const user = { userId: 1, name: "John Doe", email: "john.doe@example.com" };
+	// await dbManager.initialLoad(users);
+	
+	// await dbManager.insertUser(user);
 }
 
-function FetchDB() {
+async function FetchDB() {
+	const dbManager = new IndexedDBManager("myDatabase", "myStore");
+
+	// primeiro conecte ao banco de dados
+	await dbManager.connect();
+
+	// chame a função getAllDataFromStore para obter todos os dados na loja
+	const allData = await dbManager.getAllDataFromStore();
+
+	// imprima o resultado no console
+	console.log(allData);
     AddLogMessage('DB fetch!');
-    AddNotification('The database was fetched!', 'warning');
+    AddNotification('The database was fetched!', 'success');
 }
 
 function ClearDB() {
