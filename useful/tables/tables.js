b1fa4b468ad3f5d4a033ed945f6fc8ea2d1b301b
Fernando <fernandoavilajunior@gmail.com>
2023-05-12 21:43:38 -0300

    Table useful updated!

--- useful/tables/tables.js
+++ useful/tables/tables.js
@@ -1,30 +1,66 @@
+class Table {
+    constructor(tableId, object, parent, size = "") {
+        if (parent == null || parent == undefined || typeof parent != 'object')
+            throw new Error('The parent given is invalid');
+		if (tableId == "" || tableId == undefined || tableId == null)
+			throw new Error("The id given is invalid");
+		if (typeof object != "object")
+			throw new Error("The object given is invalid");
+		
+        this.tableId = tableId;
+		this.size = size;
+
+		this.object = object;
+
+		let table = createElement("div", [
+			{ key: "class", value: ["table"] },
+			{ key: "id", value: "table-" + tableId },
+		]);
+
+		let head = createElement("div", [
+			{ key: "class", value: ["item", "head"] },
+		]);
+
+		Object.keys(object).forEach((item) => {
+			let div = createElement("div", [{ key: "content", value: item }]);
+
+			head.appendChild(div);
+		});
+
+		table.appendChild(head);
+
+        parent.appendChild(table);
+		this.table = table;
+	}
+}
 /**
  * @description Instantiate a table with the given object keys
  * @param {string} tableId Defines the id for table div
  * @param {object} object The main table object
  */
 function CreateTable(tableId, object) {
-    if (typeof object != 'object') throw new Error('The object given is invalid');
-    let table = createElement('div', [
-        { key: 'class', value: ['table'] },
-        { key: 'id', value: "table-" + tableId }
-    ]);
+	if (tableId == "" || tableId == undefined || tableId == null)
+		throw new Error("The id given is invalid");
+	if (typeof object != "object")
+		throw new Error("The object given is invalid");
+	let table = createElement("div", [
+		{ key: "class", value: ["table"] },
+		{ key: "id", value: "table-" + tableId },
+	]);
 
-    let head = createElement('div', [
-        { key: 'class', value: ['item', 'head'] }
-    ]);
+	let head = createElement("div", [
+		{ key: "class", value: ["item", "head", "item-size-2"] },
+	]);
 
-    Object.keys(object).forEach(item => {
-        let div = createElement('div', [
-            { key: 'content', value: item }
-        ]);
+	Object.keys(object).forEach((item) => {
+		let div = createElement("div", [{ key: "content", value: item }]);
 
-        head.appendChild(div);
-    });
+		head.appendChild(div);
+	});
 
-    table.appendChild(head);
+	table.appendChild(head);
 
-    return table;
+	return table;
 }
 
 /**
@@ -32,9 +68,10 @@ function CreateTable(tableId, object) {
  * @param {object} object The main table object
  */
 function CreateTableItem(object) {
-    if (typeof object != "object") throw new Error("The object given is invalid");
-    let item = createElement("div", [
-		{ key: "class", value: ["item"] },
+	if (typeof object != "object")
+		throw new Error("The object given is invalid");
+	let item = createElement("div", [
+		{ key: "class", value: ["item", "item-size-2"] },
 	]);
 
 	Object.values(object).forEach((value) => {
@@ -42,6 +79,6 @@ function CreateTableItem(object) {
 
 		item.appendChild(div);
 	});
-    
-    return item;
+
+	return item;
 }
