51303b0288e9d7d5f0176dddfb4fded7bd368118
Fernando Avila <fernandoavilajunior@gmail.com>
2023-05-17 22:59:40 -0300

    js updated

---
+++ 14-Hello/main.js
@@ -0,0 +1,7 @@
+function LogIn() {
+    AddNotification('<hello-in-native-language> <user-name> you have successfully logged in!', 'success');
+}
+
+function LogOut() {
+    AddNotification('Have a great day <user-name>!', 'success');
+}
