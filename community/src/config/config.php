996dc1c5b444c73dbc4c78d396cffd35301954db
Fernando <fernandoavilajunior@gmail.com>
2023-05-20 00:32:03 -0300

    community configs updated

--- community/src/config/config.php
+++ community/src/config/config.php
@@ -2,5 +2,6 @@
 
 return array(
     'prefix' => '/challenges/community',
-    'template_dir' => __DIR__ . '/../templates/'
+    'template_dir' => __DIR__ . '/../templates/',
+    'base_url' => 'http://localhost/challenges/community/'
 );
