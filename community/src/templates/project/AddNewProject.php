f0797d87cda8ef4e4a5fec832bf362f27f930570
Fernando <fernandoavilajunior@gmail.com>
2023-05-24 23:56:08 -0300

    community: create new project template

--- community/src/templates/project/AddNewProject.php
+++ community/src/templates/project/AddNewProject.php
@@ -8,6 +8,41 @@
                 <h3>Create new project</h3>
             </div>
 
+            <form class="w-50" action="<?= $data['config']['base_url'] ?>/project/AddNewProject" method="post">
+                <div class="row">
+                    <div class="input-group">
+                        <input type="text" name="project_name" id="project_name" placeholder=" " required>
+                        <label for="project_name">* Project Name</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group">
+                        <textarea type="text" name="project_description" id="project_description" placeholder=" " required></textarea>
+                        <label for="project_description">Description</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group">
+                        <input type="file" name="project_file" id="project_file" required>
+                    </div>
+                </div>
+                <div class="row flex-column">
+                    <div class="input-group">
+                        <input type="radio" name="project_public" id="project_public" value="public" checked>
+                        <label for="project_public">Public</label>
+                    </div>
+                    <div class="input-group">
+                        <input type="radio" name="project_public" id="project_private" value="private">
+                        <label for="project_private">Private</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group">
+                        <input type="submit" class="btn btn-normal" value="Post project">
+                    </div>
+                </div>
+            </form>
+
         </fieldset>
     </div>
 </div>
