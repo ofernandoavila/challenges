18d09e303bdf6784b12d06579c85298b4a69a761
Fernando <fernandoavilajunior@gmail.com>
2023-05-27 14:45:59 -0300

    community: dashboard view

---
+++ community/src/templates/dashboard/DashboardPage.php
@@ -0,0 +1,50 @@
+<?php ofernandoavila\Community\Core\Template::LoadTemplatePart('common/header') ?>
+
+<div class="row justify-center align-items-start">
+    <div class="column flex flex-1">
+        <fieldset><?php ofernandoavila\Community\Core\Template::LoadTemplatePart('common/account_side_menu') ?></fieldset>
+    </div>
+    <div class="column flex flex-5">
+        <fieldset>
+            <div class="text-header flex justify-between">
+                <h3>Dashboard</h3>
+                <a class="w-20 btn btn-normal" href="<?= $data['config']['base_url'] ?>/project/add-new-project">Create new project</a>
+            </div>
+            <?php if ($data['user']->GetProjects()->count() == 0) : ?>
+                <p>No projects found.</p>
+            <?php else : ?>
+                <div class="table" id="projects">
+                    <div class="item head item-size-1">
+                        <div>ID</div>
+                        <div>Name</div>
+                        <div>Is Public?</div>
+                        <div>Actions</div>
+                    </div>
+                    <?php foreach ($data['user']->GetProjects() as $project) : ?>
+                        <div class="item item-size-1">
+                            <div>
+                                <?= $project->id; ?>
+                            </div>
+                            <div>
+                                <?= $project->name; ?>
+                            </div>
+                            <div>
+                                <?= $project->isPublic ? 'Public' : 'Private'; ?>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href="<?= $data['config']['base_url'] ?>/project/edit-project/<?= $project->id ?>">Edit</a>
+                                        <a href="<?= $data['config']['base_url'] ?>/project/delete-project/<?= $project->id ?>">Delete</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+        </fieldset>
+    </div>
+</div>
+
+<?php ofernandoavila\Community\Core\Template::LoadTemplatePart('common/footer') ?>
