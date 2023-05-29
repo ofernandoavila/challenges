2b9018d0fb95e929cd700af6b8e2832cbb1eb7ad
Fernando <fernandoavilajunior@gmail.com>
2023-05-28 23:41:58 -0300

    community: home page fixes

--- community/src/templates/Home.php
+++ community/src/templates/Home.php
@@ -1,14 +1,20 @@
 <?php ofernandoavila\Community\Core\Template::LoadTemplatePart('common/header') ?>
 
 <div class="row justify-center align-items-start">
-    <div class="column flex flex-1"><?php ofernandoavila\Community\Core\Template::LoadTemplatePart('common/aside_menu') ?></div>
     <div class="column flex flex-5">
         <fieldset>
-            
+            <div class="text-header">
+                <h3>Top games</h3>
+            </div>
+            <?php ofernandoavila\Community\Core\Template::LoadTemplatePart('components/games-list-grid') ?>
         </fieldset>
     </div>
     <div class="column flex flex-1">
-        <fieldset></fieldset>
+        <fieldset>
+            <div class="text-header">
+                <h3>Trending</h3>
+            </div>
+        </fieldset>
     </div>
 </div>
 
