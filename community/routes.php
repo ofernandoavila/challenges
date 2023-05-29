8ea5d43e947b77c1e1ec696bea9634682f452994
Fernando <fernandoavilajunior@gmail.com>
2023-05-28 23:37:58 -0300

    community: home page changes

--- community/routes.php
+++ community/routes.php
@@ -1,14 +1,19 @@
 <?php
 
+use ofernandoavila\Community\Controller\ProjectController;
 use ofernandoavila\Community\Core\BasicController;
 use ofernandoavila\Community\Core\BasicViewController;
 use ofernandoavila\Community\Core\Template;
 
 global $router;
 
-$router->get('/', function() {
+$router->get('/', function($data) {
     $controller = new BasicViewController();
-    $controller->Render('Home');
+    $projects = new ProjectController();
+
+    $data['games'] = $projects->GetProjects();
+
+    $controller->Render('Home', $data);
 });
 
 require_once __DIR__ . '/routes/DashboardRoutes.php';
