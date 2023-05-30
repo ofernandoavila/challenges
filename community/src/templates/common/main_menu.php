d0fc30d4984e3d67af4c9e933b15679c46dd834e
Fernando <fernandoavilajunior@gmail.com>
2023-05-30 19:17:53 -0300

    community: profile button added at menu bar

--- community/src/templates/common/main_menu.php
+++ community/src/templates/common/main_menu.php
@@ -3,6 +3,7 @@
 <nav>
     <ul class="row">
         <?php if (isset($_SESSION['user_session'])) : ?>
+            <li><a href="<?= $data['config']['base_url'] ?>/profile?username=<?= $data['session']->user->username; ?>">My Profile</a></li>
             <li><a href="<?= $data['config']['base_url'] ?>/my-account">My Account</a></li>
             <li><a href="<?= $data['config']['base_url'] ?>/logoff">Logoff</a></li>
         <?php else : ?>
