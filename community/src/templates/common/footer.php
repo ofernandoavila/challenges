8f9171d5383d2f101358d90b11bad7b2322ba8a9
Fernando Avila <fernandoavilajunior@gmail.com>
2023-05-31 00:05:57 -0300

    community: new scripts added

--- community/src/templates/common/footer.php
+++ community/src/templates/common/footer.php
@@ -2,7 +2,10 @@
 </div>
 </div>
 </body>
+<script src="<?= $data['config']['base_url'] ?>/public/js/Login.js"></script>
 <script src="<?= $data['config']['scripts_dir'] ?>/main.js"></script>
 <script src="<?= $data['config']['scripts_dir'] ?>/toggle-theme/toggle-theme.js"></script>
+<script src="<?= $data['config']['scripts_dir'] ?>/popup/popup.js"></script>
+<script src="<?= $data['config']['scripts_dir'] ?>/forms/forms.js"></script>
 
 </html>
