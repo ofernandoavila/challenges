407ecd094d52e8f2cfdbb862ba000b7d426adf4d
Fernando Avila <fernandoavilajunior@gmail.com>
2023-05-31 01:47:21 -0300

    community: profile page fixes

--- community/src/Controller/ProfileController.php
+++ community/src/Controller/ProfileController.php
@@ -13,12 +13,21 @@ class ProfileController extends Controller {
 
     public function ProfilePage($data = []) {
 
-        $user = new UserController();
+        $userController = new UserController();
         $project = new ProjectController();
 
-        $data['user'] = $user->GetUserByUsername($data['username']);
 
-        if($data['user'] != null) $data['projects'] = $project->GetProjectsByUser($data['user']);
+        $data['profile_user'] = $userController->GetUserByUsername($data['username']);
+
+        if(isset($data['user'])) {
+            $currentUser = $userController->GetUserByID($data['user']->id);
+            
+            $data['isFollowing'] = $userController->IsFollowing($currentUser, $data['profile_user']);
+        } else {
+            $data['isFollowing'] = false;
+        }
+
+        if($data['profile_user'] != null) $data['projects'] = $project->GetProjectsByUser($data['profile_user']);
 
         return $this->render('profile/ProfilePage', $data);
     }
--- community/src/templates/profile/ProfilePage.php
+++ community/src/templates/profile/ProfilePage.php
@@ -1,7 +1,40 @@
 <?php global $data; ?>
 <?php ofernandoavila\Community\Core\Template::LoadTemplatePart('common/header') ?>
 
-<?php if($data['user'] != null): ?>
+<?php if($data['profile_user'] != null): ?>
+    <script>
+        
+        async function Follow(currentUserId, targetId) {
+            if(currentUserId == null) return Login.openLoginPopup();
+            
+            await fetch('<?= $data['config']['base_url'] ?>/follow', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'userId': currentUserId,
+                    'targetUserId': targetId
+                })
+            }).then(res => res.json())
+            .then(data => {
+                let btn = document.querySelector('#follow-button');
+                btn.setAttribute("data-following", data.following.toString());
+
+                let currentValue = parseInt(document.querySelector('#followers-total').innerHTML);
+
+                if(data.following) {
+                    document.querySelector('#follow-button').innerHTML = "Following";
+                    document.querySelector('#followers-total').innerHTML = currentValue + 1;
+                } else {
+                    document.querySelector('#follow-button').innerHTML = "Follow";
+                    document.querySelector('#followers-total').innerHTML = currentValue - 1;
+                }
+
+            });
+        }
+    </script>
     <div class="row justify-center align-items-start">
         <div class="column flex flex-1">
             <fieldset>
@@ -14,30 +47,46 @@
                             <img src="http://ofernandoavila.avilamidia.com/wp-content/uploads/2022/04/cropped-138798512_3706164436112179_1414491971049075834_n.jpg" alt="">
                         </div>
                         <div class="post-meta-author-info">
-                            <h3>@<?= $data['user']->username; ?></h3>
+                            <h3>@<?= $data['profile_user']->username; ?></h3>
                             <span>Administrator</span>
                         </div>
                         <div class="row mg-top-20">
-                            <?php if(isset($data['session']->user->username) && $data['session']->user->username == $data['user']->username): ?>
+                            <?php if(isset($data['session']->user->username) && $data['session']->user->username == $data['profile_user']->username): ?>
                                 <a href="<?= $data['config']['base_url'] ?>/my-account/edit-account"><button class="btn btn-default" value="config">Manage profile</button></a>
                             <?php else: ?>
-                                <button class="btn btn-normal" value="plus">Follow</button>
+                                <button
+                                    id="follow-button"
+                                    data-following="<?= $data['isFollowing'] ? 'true' : 'false' ?>"
+                                    class="btn btn-normal w-100"
+                                    value="plus"
+                                    onclick="Follow(<?= $data['user']->id ?? 'null'; ?>, <?= $data['profile_user']->id; ?>)"
+                                ><?= $data['isFollowing'] ? 'Following' : 'Follow' ?></button>
                             <?php endif; ?>
+                        </div>
+                        <div class="row">
+                            <div class="columm">
+                                <div class="row"><h5>Followers</h5></div>
+                                <div class="row" id="followers-total"><?= $data['profile_user']->GetFollowersCount() ?></div>
+                            </div>
+                            <div class="columm">
+                                <div class="row"><h5>Following</h5></div>
+                                <div class="row" id="followings-total"><?= $data['profile_user']->GetFollowingsCount() ?></div>
+                            </div>
                         </div>
                     </div>
                     <hr>
                     <div class="post-meta-data">
                         <div class="row">
                             <div class="column">Projects</div>
-                            <div class="column"><?= sizeof($data['user']->GetProjects()); ?></div>
+                            <div class="column"><?= sizeof($data['profile_user']->GetProjects()); ?></div>
                         </div>
                         <div class="row">
                             <div class="column">Projects Likes</div>
-                            <div class="column"><?= $data['user']->TotalProjectsLikes(); ?></div>
+                            <div class="column"><?= $data['profile_user']->TotalProjectsLikes(); ?></div>
                         </div>
                         <div class="row">
                             <div class="column">Faves</div>
-                            <div class="column"><?= $data['user']->GetTotalFaves(); ?></div>
+                            <div class="column"><?= $data['profile_user']->GetTotalFaves(); ?></div>
                         </div>
                     </div>
                 </div>
